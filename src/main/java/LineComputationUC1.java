ef9911683ac1b67cab75a73f212110beedb40a7f
amruta.vatare <vatareamruta615@gmail.com>
2022-08-19 14:32:52 +0530

    UC1 implemented

--- src/main/java/LineComputationUC1.java
+++ src/main/java/LineComputationUC1.java
@@ -1,8 +1,17 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class LineComputationUC1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Line Computation");
+        DecimalFormat df = new DecimalFormat("#.00");
+        System.out.println("Enter X1 and Y1 co-ordinate for line");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        System.out.println("Enter X2 and Y2 co-ordinate for line");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        double length = Math.sqrt(Math.pow((x2-x1),2) +Math.pow((y2-y1),2));
+        System.out.println("Length of given line co-ordinate ("+x1+","+y1+") ("+x2+","+y2+") is: "+df.format(length));
     }
 }
